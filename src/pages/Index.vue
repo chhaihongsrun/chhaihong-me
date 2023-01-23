de0d52328a883004d7f88fcde88e2f9ad209bd5f
Chhaihong <chhaihong.dev@gmail.com>
2023-01-23 20:46:57 +0700

    Updated for 2023

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,17 @@
 <template>
   <Layout>
-    <div class="text-teal-700 mt-6 lg:mt-12">
+    <div class="mt-6 text-teal-700 lg:mt-12">
       <ul class="list-inside">
         <li class="mt-6">
-          <h2 class="text-xl lg:text-2xl text-gray-700">A little bit about me</h2>
+          <h2 class="text-xl text-gray-700 lg:text-2xl">A little bit about me</h2>
           <p class="text-base text-gray-500">A am a developer based in Cambodia. I am interested in anything that is related to Computer, from Backend to Frontend.</p>
         </li>
         <li class="mt-6">
-          <h2 class="text-xl lg:text-2xl text-gray-700">A little more?</h2>
-          <p class="text-base text-gray-500">Things with me everyday are 15.4" MacBook Pro mid 2019 running latest version of macOS.</p>
+          <h2 class="text-xl text-gray-700 lg:text-2xl">A little more?</h2>
+          <p class="text-base text-gray-500">Things with me everyday is Lenovo ThinkPad T490 running the latest version of Windows 11.</p>
         </li>
       </ul>
     </div>
-
   </Layout>
 </template>
 
--- src/pages/Metadata.vue
+++ src/pages/Metadata.vue
@@ -1,61 +1,57 @@
 <template>
   <Layout>
-    <div class="text-gray-600 mt-6 lg:mt-12">
+    <div class="mt-6 text-gray-600 lg:mt-12">
       <h1 class="text-xl text-gray-400">chhaihongsrun[at]gmail[dot]com</h1>
+
       <ul class="list-inside">
-        <li class="mt-6 border-l-2 border-teal-600 pl-2">
-          <h2 class="leading-none mb-2 text-base text-gray-400">Now</h2>
-          <p class="leading-none text-base text-gray-400">Freelancer</p>
-          <div class="overflow-x-scroll flex items-center w-full h-8 font-light text-sm mt-4">
-            <a href="https://www.w3schools.com/whatis/whatis_fullstack.asp" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-400 rounded-full text-gray-400 hover:text-gray-200">Full stack web developer</a>
+        <li class="pl-2 mt-6 border-l-2 border-teal-600">
+          <h2 class="mb-2 text-base leading-none text-gray-400">Jan 2021 - Now</h2>
+          <p class="text-base leading-none text-gray-400">Freelance</p>
+          <div class="mt-2.5 flex flex-wrap items-center space-y-1.5 space-x-2.5 text-sm font-light">
+            <a href="https://www.w3schools.com/whatis/whatis_fullstack.asp" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-400 rounded-full hover:text-gray-200">Full Stack Web Developer</a>
           </div>
         </li>
-        <li class="mt-6 border-l-2 border-gray-600 pl-2">
-          <h2 class="leading-none mb-2 text-base text-teal-600">Feb 2019 - Jan 2021</h2>
-          <p class="leading-none text-base text-teal-700">Software Engineer at Vodworks</p>
-          <div class="overflow-x-scroll flex items-center w-full h-8 font-light text-sm mt-4">
-            <a href="https://microservices.io/" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Microservices</a>
-            <a href="https://php.net/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">PHP</a>
-            <a href="https://www.typescriptlang.org/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">TypeScript</a>
-            <a href="https://www.mysql.com/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">MySQL</a>
-            <a href="https://cassandra.apache.org/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Cassandra</a>
-            <a href="https://www.docker.com/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Docker</a>
-            <a href="https://en.wikipedia.org/wiki/CI/CD/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">CI/CD</a>
+        <li class="pl-2 mt-6 border-l-2 border-gray-600">
+          <h2 class="mb-2 text-base leading-none text-teal-600">Feb 2019 - Jan 2021</h2>
+          <p class="text-base leading-none text-teal-700">Software Engineer at Vodworks</p>
+          <div class="mt-2.5 flex flex-wrap items-center space-y-1.5 space-x-2.5 text-sm font-light">
+            <a href="https://microservices.io/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Microservices</a>
+            <a href="https://php.net/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">PHP</a>
+            <a href="https://www.typescriptlang.org/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">TypeScript</a>
+            <a href="https://www.mysql.com/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">MySQL</a>
+            <a href="https://cassandra.apache.org/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Cassandra</a>
+            <a href="https://www.docker.com/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Docker</a>
+            <a href="https://en.wikipedia.org/wiki/CI/CD/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">CI/CD</a>
           </div>
         </li>
-        <li class="mt-8 border-l-2 border-gray-600 pl-2">
-          <h2 class="leading-none mb-2 text-base text-gray-600">May 2018 - Feb 2019</h2>
-          <p class="leading-none text-base">Web Developer at Okoone</p>
-          <div class="overflow-x-scroll flex items-center w-full h-8 font-light text-sm mt-2">
-            <a href="https://wordpress.org/" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Wordpress</a>
-            <a href="https://www.typescriptlang.org/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">TypeScript</a>
+        <li class="pl-2 mt-8 border-l-2 border-gray-600">
+          <h2 class="mb-2 text-base leading-none text-gray-600">May 2018 - Feb 2019</h2>
+          <p class="text-base leading-none">Web Developer at Okoone</p>
+          <div class="mt-2.5 flex flex-wrap items-center space-y-1.5 space-x-2.5 text-sm font-light">
+            <a href="https://www.typescriptlang.org/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">TypeScript</a>
+            <a href="https://angular.io/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Angular</a>
+            <a href="https://vuejs.org/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">VueJS</a>
+            <a href="https://php.net/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">PHP</a>
+            <a href="https://laravel.com/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Laravel</a>
+            <a href="https://wordpress.org/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Wordpress</a>
           </div>
         </li>
-        <li class="mt-8 border-l-2 border-gray-600 pl-2">
-          <h2 class="leading-none mb-2 text-base text-gray-600">Jul 2017 - May 2018</h2>
-          <p class="leading-none text-base">Web Developer at Marina Inc</p>
-          <div class="overflow-x-scroll flex items-center w-full h-8 font-light text-sm mt-4">
-            <a href="https://laravel.com/" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Laravel</a>
-            <a href="https://www.ec-cube.net/" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">EC-CUBE</a>
-            <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">JavaScript</a>
+        <li class="pl-2 mt-8 border-l-2 border-gray-600">
+          <h2 class="mb-2 text-base leading-none text-gray-600">Jul 2016 - May 2018</h2>
+          <p class="text-base leading-none">Web Developer at Marina Inc</p>
+          <div class="mt-2.5 flex flex-wrap items-center space-y-1.5 space-x-2.5 text-sm font-light">
+            <a href="https://laravel.com/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Laravel</a>
+            <a href="https://www.ec-cube.net/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">EC-CUBE</a>
+            <a href="https://en.wikipedia.org/wiki/JavaScript" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">JavaScript</a>
           </div>
         </li>
-        <li class="mt-8 border-l-2 border-gray-600 pl-2">
-          <h2 class="leading-none mb-2 text-base text-gray-600">Aug 2016 - Jun 2017</h2>
-          <p class="leading-none text-base">Web Developer at Today Communication Co, Ltd</p>
-          <div class="overflow-x-scroll flex items-center w-full h-8 font-light text-sm mt-4">
-            <a href="https://wordpress.org/" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Wordpress</a>
-            <a href="https://www.adobe.com/products/illustrator.html" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Illustrator</a>
-            <a href="https://www.adobe.com/products/photoshop.html" target="_blank" rel="nofollow" class=" py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Photoshop</a>
-          </div>
-        </li>
-        <li class="mt-8 border-l-2 border-gray-600 pl-2">
-          <h2 class="leading-none mb-2 text-base text-gray-600">Jul 2014 - Jul 2016</h2>
-          <p class="leading-none text-base">IT Officer at Zaman International School</p>
-          <div class="overflow-x-scroll flex items-center w-full h-8 font-light text-sm mt-4">
-            <a href="https://wordpress.org/" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Wordpress</a>
-            <a href="https://www.adobe.com/products/illustrator.html" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Illustrator</a>
-            <a href="https://www.adobe.com/products/photoshop.html" target="_blank" rel="nofollow" class="py-1 px-2 mr-2 border border-gray-600 rounded-full text-gray-400 hover:text-gray-200">Photoshop</a>
+        <li class="pl-2 mt-8 border-l-2 border-gray-600">
+          <h2 class="mb-2 text-base leading-none text-gray-600">Jul 2014 - Jul 2016</h2>
+          <p class="text-base leading-none">IT Officer at Zaman International School</p>
+          <div class="mt-2.5 flex flex-wrap items-center space-y-1.5 space-x-2.5 text-sm font-light">
+            <a href="https://wordpress.org/" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Wordpress</a>
+            <a href="https://www.adobe.com/products/illustrator.html" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Illustrator</a>
+            <a href="https://www.adobe.com/products/photoshop.html" target="_blank" rel="nofollow" class="px-2 py-1 mr-2 text-gray-400 border border-gray-600 rounded-full hover:text-gray-200">Photoshop</a>
           </div>
         </li>
       </ul>
